218dece3475b21340f71f5dfbb95d1244b5778bc
sxxk2 <sxxklee@gmail.com>
2022-01-13 14:32:32 +0900

    ADD: ProductDetailView

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+import json
+
 from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q, Min, Max
 
-from products.models import Product, Brand, Size
+from products.models import Product, Brand, Size, Image
 
 class ProductListView(View):
     def get(self, request):
@@ -38,7 +40,7 @@ class ProductListView(View):
             "low_price"  : "price",
             "high_price" : "-price"
         }
-        
+
         ordering = ordering_field.get(order, "-release_at")
 
         result = [{
@@ -65,9 +67,37 @@ class FilterBarView(View):
                 } for brand in brands],
             "size"  : [
                 {
-                    "id"   : size.id, 
+                    "id"   : size.id,
                     "name" : size.name
                 } for size in sizes]
             }
 
         return JsonResponse({"result" : result}, status=200)
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        if not Product.objects.filter(id=product_id).exists():
+            return JsonResponse({'message' : 'PRODUCT_DOES_NOT_EXISTS'}, status=404)
+
+        product = Product.objects.get(id=product_id)
+
+        images  = product.image_set.all()
+        image_urls = [image.url for image in images]
+        image_urls.insert(0,product.thumbnail_url)
+
+        like_count = product.like_set.filter(is_like=True).count()
+
+        result = {
+            'product_id'     : product.id,
+            'brand'          : product.brand.name,
+            'name'           : product.name,
+            'korean_name'    : product.korean_name,
+            'model_number'   : product.model_number,
+            'color'          : product.color,
+            'release_at'     : product.release_at,
+            'retail_price'   : int(product.retail_price),
+            'image_urls'     : image_urls,
+            'like_count'     : like_count,
+            'is_like'        : False
+        }
+        return JsonResponse({'result' : result}, status = 200)
