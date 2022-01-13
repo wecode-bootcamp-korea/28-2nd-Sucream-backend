e426668505d190d0362826691ccf1669622044ad
sxxk2 <sxxklee@gmail.com>
2022-01-13 11:52:03 +0900

    users/views.py 카카오 로그인/회원가입 로직 구현, users/test.py 카카오 로직 테스트코드 구현,  cores/utils.py 로그인 데코레이터 구현후 로그인 데코레이터 리뷰 수정사항 반영

--- cores/utils.py
+++ cores/utils.py
@@ -1,2 +1,26 @@
+import json
+import jwt
+
+from django.http  import JsonResponse
+
+from my_settings  import SECRET_KEY, ALGORITHM
+from users.models import User
+
 def login_decorator():
-    pass
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers['Authorization']
+            payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+            user         = User.objects.get(id=payload['user_id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+        return func(self, request,  *args, **kwargs)
--- users/views.py
+++ users/views.py
@@ -1,1 +1,29 @@
-from django.shortcuts import render
+import requests, jwt
+
+from django.http.response import JsonResponse
+from django.conf          import settings
+from django.views         import View
+
+from my_settings       import SECRET_KEY, ALGORITHM
+from users.models      import User
+
+class KakaoLoginView(View):
+    def get(self, request):
+        try:
+            kakao_token   = request.headers['Authorization']
+            kakao_account = requests.get('https://kapi.kakao.com/v2/user/me', headers={'Authorization': f'Bearer {kakao_token}'}).json()
+
+            if not User.objects.filter(kakao_id=kakao_account['id']).exists():
+                user = User.objects.create(
+                    kakao_id = kakao_account['id'],
+                    email    = kakao_account['kakao_account']['email']
+                )
+            else:
+                user = User.objects.get(kakao_id=kakao_account['id'])
+
+            access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({'access_token': access_token}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message': 'AUTHORIZATION_KEY_ERROR'}, status=400)
