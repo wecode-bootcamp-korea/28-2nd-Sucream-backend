93a6e64912cce4d3c0e85c0581b24acbdb0c475b
sxxk2 <sxxklee@gmail.com>
2022-01-13 14:32:32 +0900

    ADD: ProductDetailView

--- products/tests.py
+++ products/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase, Client
 
 from products.models import Product, Brand, Size, ProductSize
-from biddings.models import Bidding
-from users.models    import User
+from biddings.models import Bidding, Order, Status
+from users.models    import User, Like
 
 class ProductListUnitTest(TestCase):
     def setUp(self):
@@ -78,11 +78,11 @@ class ProductListUnitTest(TestCase):
         Size.objects.all().delete()
         ProductSize.objects.all().delete()
         Bidding.objects.all().delete()
-    
+
     def test_productlistview_get_success(self):
         client = Client()
         response = client.get('/products')
-        
+
         result = [
             {
                 "id"            : 1,
@@ -108,7 +108,7 @@ class ProductListUnitTest(TestCase):
     def test_productlistview_get_brand_filter_success(self):
         client = Client()
         response = client.get('/products?brand=1')
-        
+
         result = [
             {
                 "id"            : 1,
@@ -126,7 +126,7 @@ class ProductListUnitTest(TestCase):
     def test_productlistview_get_size_filter_success(self):
         client = Client()
         response = client.get('/products?size=2')
-        
+
         result = [
             {
                 "id"            : 2,
@@ -144,7 +144,7 @@ class ProductListUnitTest(TestCase):
     def test_productlistview_get_order_success(self):
         client = Client()
         response = client.get('/products?sort=highest')
-        
+
         result = [
             {
                 "id"            : 2,
@@ -185,7 +185,7 @@ class FilterBarUnitTest(TestCase):
     def test_filterbarview_get_success(self):
         client = Client()
         response = client.get('/products/filter')
-        
+
         result = {
             "brand" : [
                 {
@@ -209,3 +209,116 @@ class FilterBarUnitTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"result" : result})
+
+class ProductDetailTest(TestCase):
+    def setUp(self):
+        User.objects.bulk_create([
+            User(
+                id       = 1,
+                kakao_id = 1
+            ),
+            User(
+                id       = 2,
+                kakao_id = 2
+            )
+        ])
+
+        Brand.objects.create(
+            id   = 1,
+            name = "Nike"
+        )
+
+        Product.objects.create(
+            id            = 1,
+            name          = "Nike Dunk Low Retro Black",
+            korean_name   = "나이키 덩크 로우 레트로 블랙",
+            model_number  = "DD1391-100",
+            color         = "WHITE/BLACK",
+            release_at    = "2021-01-14",
+            retail_price  = 129000,
+            thumbnail_url = 'https://testimgurl.com',
+            brand_id      = 1
+        )
+
+        Like.objects.create(
+            id         = 1,
+            is_like    = 1,
+            user_id    = 1,
+            product_id = 1
+        )
+
+        Size.objects.create(
+            id   = 1,
+            name = '260'
+        )
+
+        Status.objects.create(
+            id   = 1,
+            name = 'Nike'
+        )
+
+        Bidding.objects.bulk_create([
+            Bidding(
+                id                = 1,
+                price             = 10000,
+                is_buyer          = 0,
+                user_id           = 1,
+                products_sizes_id = 1
+            ),
+            Bidding(
+                id                = 2,
+                price             = 10000,
+                is_buyer          = 1,
+                user_id           = 2,
+                products_sizes_id = 1
+            )
+        ])
+
+        Order.objects.create(
+            id                = 1,
+            buyer_id          = 2,
+            seller_id         = 1,
+            price             = 10000,
+            products_sizes_id = 1,
+            status_id         = 1
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Brand.objects.all().delete()
+        Like.objects.all().delete()
+        Size.objects.all().delete()
+        Status.objects.all().delete()
+        Bidding.objects.all().delete()
+        Order.objects.all().delete()
+        Product.objects.all().delete()
+
+    def test_productdetailview_get_error(self):
+        client = Client()
+        response = client.get('/products/2')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), {'message' : 'PRODUCT_DOES_NOT_EXISTS'})
+
+    def test_productdetailgraphview_get_success(self):
+        client   = Client()
+        response = client.get('/products/1/graph')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+                'result' : [{
+                    'id'         : 1,
+                    'size'       : '260',
+                    'price'      : 10000,
+                    'created_at' : '2022/01/18'
+                }]
+            }
+        )
+
+    def test_productdetailgraphview_get_orderdoesnotexist(self):
+        client = Client()
+        response = client.get('/products/2/graph')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'message' : 'INVALID_ORDER'})
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from products.views import ProductListView, FilterBarView
+from products.views import ProductListView, FilterBarView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/filter', FilterBarView.as_view()),
-    
+    path('/<int:product_id>', ProductDetailView.as_view()),
 ]
