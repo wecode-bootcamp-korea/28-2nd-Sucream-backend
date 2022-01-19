c6b277408bbbdead6bc94d76cf26300ff1f58ba6
sxxk2 <sxxklee@gmail.com>
2022-01-19 21:25:44 +0900

    ADD: NewBiddingView

--- biddings/views.py
+++ biddings/views.py
@@ -1,8 +1,13 @@
+import json, jwt
+
 from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Max, Min
 
-from products.models import Product, ProductSize
+from products.models import Product, Size, ProductSize
+from users.models    import User
+from biddings.models import Bidding
+from cores.utils     import login_decorator
 
 class BiddingView(View):
     def get(self, request):
@@ -23,7 +28,7 @@ class BiddingView(View):
 
         instant_buy  = min([int(product.price) for product in buy_products if product.price])
         instant_sell = max([int(product.price) for product in sell_products if product.price])
-            
+
         result = {
             "instant_buy"  : instant_buy,
             "instant_sell" : instant_sell,
@@ -51,3 +56,31 @@ class BiddingView(View):
         }
 
         return JsonResponse({"result" : result}, status=200)
+
+class NewBiddingView(View):
+    @login_decorator
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+
+            user     = request.user
+            is_buyer = data['is_buyer']
+            price    = data['offered_price']
+            product  = data['id']
+            size     = data['size']
+
+            if not ProductSize.objects.filter(size__name = size, product_id = product).exists():
+                return JsonResponse({'message' : 'PRODUCTSIZE_DOES_NOT_EXIST'}, status=204)
+
+            product_size = ProductSize.objects.get(size__name = size, product_id = product)
+
+            Bidding.objects.create(
+                user_id           = user.id,
+                is_buyer          = is_buyer,
+                price             = price,
+                products_sizes_id = product_size.id
+            )
+
+            return JsonResponse({'message' : 'OFFER_SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
