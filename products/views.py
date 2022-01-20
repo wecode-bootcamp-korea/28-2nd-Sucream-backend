c1eda7e3827fe6d5f2eb8e87860830f8b8c11fce
nine57 <gusrn015@gmail.com>
2022-01-20 15:25:27 +0900

    Modify: productlist query parameter get code changed

--- products/views.py
+++ products/views.py
@@ -1,19 +1,16 @@
-import json
-
 from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q, Min, Max
 
-from products.models import Product, Brand, Size, Image
-from biddings.models import Bidding, Order
+from products.models import Product, Brand, Size
+from biddings.models import Order
 
 class ProductListView(View):
     def get(self, request):
         brands   = request.GET.getlist('brand', None)
-        sizes    = request.GET.getlist('size', None)
+        sizes    = request.GET.get('size', None)
         order    = request.GET.get('order', 'recent')
         is_buyer = request.GET.get('is_buyer', 1)
-
         is_buyer = bool(int(is_buyer))
 
         filtering = Q()
@@ -22,7 +19,8 @@ class ProductListView(View):
             filtering &= Q(brand__id__in = brands)
 
         if sizes:
-            filtering &= Q(sizes__id__in = sizes)
+            size_list  = sizes.split(',')
+            filtering &= Q(sizes__id__in = size_list)
 
         if is_buyer:
             filtering &= Q(productsize__bidding__is_buyer = True)
@@ -82,31 +80,30 @@ class ProductDetailView(View):
 
         product = Product.objects.get(id=product_id)
 
-        images  = product.image_set.all()
+        images     = product.image_set.all()
         image_urls = [image.url for image in images]
         image_urls.insert(0,product.thumbnail_url)
 
         like_count = product.like_set.filter(is_like=True).count()
 
         result = {
-            'product_id'     : product.id,
-            'brand'          : product.brand.name,
-            'name'           : product.name,
-            'korean_name'    : product.korean_name,
-            'model_number'   : product.model_number,
-            'color'          : product.color,
-            'release_at'     : product.release_at,
-            'retail_price'   : int(product.retail_price),
-            'image_urls'     : image_urls,
-            'like_count'     : like_count,
-            'is_like'        : False
+            'product_id'   : product.id,
+            'brand'        : product.brand.name,
+            'name'         : product.name,
+            'korean_name'  : product.korean_name,
+            'model_number' : product.model_number,
+            'color'        : product.color,
+            'release_at'   : product.release_at,
+            'retail_price' : int(product.retail_price),
+            'image_urls'   : image_urls,
+            'like_count'   : like_count,
+            'is_like'      : False
         }
         return JsonResponse({'result' : result}, status = 200)
 
 class ProductDetailGraphView(View):
     def get(self, request, product_id):
         try:
-
             result = [{
                 'id'         : order.id,
                 'size'       : order.products_sizes.size.name,
@@ -115,5 +112,6 @@ class ProductDetailGraphView(View):
             } for order in Order.objects.filter(products_sizes__product_id=product_id).order_by('-created_at')]
 
             return JsonResponse({'result' : result}, status = 200)
+            
         except Order.DoesNotExist:
             return JsonResponse({'message' : 'INVALID_ORDER'}, status=401)
